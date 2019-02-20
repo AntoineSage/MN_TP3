e8d88adf2335212aa5827038bc715228c61e350b
rollinal <alexis.rollin29@orange.fr>
2019-02-20 17:15:25 +0100

    Debut MN

---
+++ TP2_BLAS/src/axpy.c
@@ -0,0 +1,56 @@
+#include "mnblas.h"
+#include "complexe.h"
+
+
+void cblas_saxpy (const int N, const float a, const float *X, const int incX, float *Y, const int incY){
+
+  register unsigned int i = 0 ;
+	register unsigned int j = 0 ;
+
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      Y[j] = a*X[i] ;
+    }
+
+  return ;
+}
+
+void cblas_daxpy (const int N, const double a, const double *X, const int incX, double *Y, const int incy){
+
+  register unsigned int i = 0 ;
+	register unsigned int j = 0 ;
+
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      Y[j] = a*X[i] ;
+    }
+
+  return ;
+}
+
+void cblas_caxpy (const int N, const void *a, const void *X, const int incX, void *Y, const int incy){
+
+  register unsigned int i = 0 ;
+	register unsigned int j = 0 ;
+
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      ((complexe_float_t*)Y)[j] = a*((complexe_float_t*)X)[i] ;
+    }
+
+  return ;
+}
+
+void cblas_zaxpy (const int N, const void *a, const void *X, const int incX, void *Y, const int incy){
+
+  register unsigned int i = 0 ;
+	register unsigned int j = 0 ;
+
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      ((complexe_double_t*)Y)[j] = a*((complexe_double_t*)X)[i] ;
+    }
+
+  return ;
+}
+
