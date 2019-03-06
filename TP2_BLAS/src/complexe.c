15aec35258352afe26401b4f7aa8d95ed9a88030
Antoine Saget <antoinesaget19@gmail.com>
2019-03-06 15:08:35 +0100

    Correction problèmes complexe.c

--- TP2_BLAS/src/complexe.c
+++ TP2_BLAS/src/complexe.c
@@ -19,23 +19,22 @@ complexe_double_t add_complexe_double(const complexe_double_t c1, const complexe
 	return r;
 }
 
-complexe_float_t mult_scalaire_float(const complexe_float_t c, const float a){
+complexe_float_t mult_scalaire_float(const complexe_float_t c, const float a) {
 	complexe_float_t r;
-	r.real = a*c.real;
-	r.imaginary = a*c.imaginary;
+	r.real = a * c.real;
+	r.imaginary = a * c.imaginary;
 	return r;
 }
 
-complexe_double_t mult_scalaire_double(const complexe_double_t c, const double a){
+complexe_double_t mult_scalaire_double(const complexe_double_t c, const double a) {
 	complexe_double_t r;
-	r.real = a*c.real;
-	r.imaginary = a*c.imaginary;
+	r.real = a * c.real;
+	r.imaginary = a * c.imaginary;
 	return r;
 }
 
-complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
-{
-  complexe_float_t r ;
+complexe_float_t mult_complexe_float(const complexe_float_t c1, const complexe_float_t c2) {
+	complexe_float_t r;
 
 	r.real = (c1.real * c2.real) - (c1.imaginary * c2.imaginary);
 	r.imaginary = (c1.real * c2.imaginary) + (c1.imaginary * c2.real);
@@ -71,18 +70,21 @@ complexe_double_t mult_complexe_conjug_double(const complexe_double_t c1,
 	return r;
 }
 
-void init_complexe_double(const int N, const double *a, const double *b, const void *res){
-	
-	register unsigned int i = 0 ;
+void init_complexe_double(const int N, const double *a, const double *b, const void *res) {
+	register unsigned int i = 0;
 
-	for (; (i < N); i++)
-    {
-    	(((complexe_double_t*)res)[i]).real = a[i];
-		(((complexe_double_t*)res)[i]).imaginary = b[i];
-    }
+	for (; (i < N); i++) {
+		(((complexe_double_t *)res)[i]).real = a[i];
+		(((complexe_double_t *)res)[i]).imaginary = b[i];
+	}
 
-	return ;
+	return;
 }
+
+int complexe_equal_float(const complexe_float_t a, const complexe_float_t b) {
+	return (a.real == b.real) && (a.imaginary == b.imaginary);
+}
+
 
 int complexe_equal_double(const complexe_double_t a, const complexe_double_t b) {
 	return (a.real == b.real) && (a.imaginary == b.imaginary);
