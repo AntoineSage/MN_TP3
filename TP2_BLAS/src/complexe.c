0f453c74b2d0d25c67da3526a82eb52f9b4d0698
rollinal <alexis.rollin29@orange.fr>
2019-03-06 15:09:31 +0100

    correction

--- TP2_BLAS/src/complexe.c
+++ TP2_BLAS/src/complexe.c
@@ -84,6 +84,10 @@ void init_complexe_double(const int N, const double *a, const double *b, const v
 	return ;
 }
 
+int complexe_equal_float(const complexe_float_t a, const complexe_float_t b) {
+	return (a.real == b.real) && (a.imaginary == b.imaginary);
+}
+
 int complexe_equal_double(const complexe_double_t a, const complexe_double_t b) {
 	return (a.real == b.real) && (a.imaginary == b.imaginary);
 }
